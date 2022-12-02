8e8d56cd9bc8b7b7a66ada122164942be8e5b366
Thiện Khải <thienkhaiak5555@gmail.com>
2022-12-02 21:26:12 +0700

    search api, nodemon

--- controllers/CauLacBoController.js
+++ controllers/CauLacBoController.js
@@ -40,7 +40,17 @@ const CauLacBo = {
         } catch (error) {
             res.status(500).send(error);
         }
+    },
+    SearchCauLacBo: async (req, res) => {
+        const data = await CauLacBoModel.find({
+            "$or": [
+                { TENCLB: { $regex: req.params.key, $options: 'i' } },
+                { SANVANDONG: { $regex: req.params.key, $options: 'i' } }
+            ]
+        })
+        res.send(data)
     }
+
 }
 
 module.exports = CauLacBo
--- routes/HuanLuyenVienRouter.js
+++ routes/HuanLuyenVienRouter.js
@@ -7,5 +7,6 @@ router.post('/taohuanluyenvien', upload.single('AVATAR'), HuanLuyenVienControlle
 router.get('/gethuanluyenvien', HuanLuyenVienController.GetHuanLuyenVien);
 router.patch('/updatehuanluyenvien/:id', HuanLuyenVienController.UpdateHuanLuyenVien);
 router.delete('/deletehuanluyenvien/:id', HuanLuyenVienController.DeleteHuanLuyenVien);
+router.get('/search/:key', HuanLuyenVienController.SearchHuanLuyenVien);
 
 module.exports = router
