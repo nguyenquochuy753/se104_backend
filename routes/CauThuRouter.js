2903461603671ddef4055d008bdf5d5bf2781e47
mchis1908 <mchis19.08@gmail.com>
2022-12-05 12:04:41 +0700

    updated

--- routes/CauThuRouter.js
+++ routes/CauThuRouter.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const CauThuController = require('../controllers/CauThuController');
+const upload = require("../models/middleware/uploadImage");
 
-
-router.post('/taocauthu', CauThuController.TaoCauThu);
+router.post("/taocauthu", upload.single("AVATAR"), CauThuController.TaoCauThu);
 router.get('/getcauthu', CauThuController.GetCauThu);
 router.patch('/updatecauthu/:id', CauThuController.UpdateCauThu);
 router.delete('/deletecauthu/:id', CauThuController.DeleteCauThu);
