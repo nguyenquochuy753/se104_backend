10c32101ffb3e1bf433015501e4788177d24cb47
nguyenquochuy753 <92136604+nguyenquochuy753@users.noreply.github.com>
2022-12-17 21:02:18 +0700

    11111

--- models/CauThu.js
+++ models/CauThu.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
 const CauThuSchema = mongoose.Schema({
+    MAMG: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'MuaGiai',
+        required: true
+    },
     MACLB: {
         type: mongoose.Schema.Types.ObjectId,
         ref:'CauLacBo',
